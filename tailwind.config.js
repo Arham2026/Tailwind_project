986c2c869244c8efb865e67c4046799fe0147ef4
Arham Shajahan <universalmadman@gmail.com>
2024-01-25 07:45:09 +0530

    this is the first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// here we are exporting the modules so we are entering the directory Here
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
